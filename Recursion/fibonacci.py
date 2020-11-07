a261ae7b2933d9bc246d8b6ce69f412a9913e68a
Nitin <er.nitinpatel914@gmail.com>
2020-11-06 21:36:35 -0500

    Add time complexity.

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -5,14 +5,14 @@ The Fibonacci Sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,...
 """
 
 
-def fibonacci_iterative(n):
+def fibonacci_iterative(n):  # O(n)
     l = [0, 1]
     for i in range(2, n+1):
         l.append(l[i-1] + l[i-2])
     return l[n]
 
 
-def fibonacci_recursive(n):
+def fibonacci_recursive(n):  # O(2^n)
     if n < 2:
         return n
     return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
